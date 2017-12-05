3b7368032bd7cd237e7e544208862a4d6ddbd694
nzambello <nicola.zambello@redturtle.it>
2017-12-05 11:46:48 +0100

    reverted normals handling test

--- src/esercizio1.js
+++ src/esercizio1.js
@@ -65,34 +65,6 @@ const FSHADER_SOURCE = `
   }
 `
 
-const cross = (edge1, edge2) => {
-  let n = new Array(3)
-  /*
-   * Nx = UyVz - UzVy
-   * Ny = UzVx - UxVz
-   * Nz = UxVy - UyVx
-   */
-  n[0] = edge1[1] * edge2[2] - edge1[2] * edge2[1]
-  n[1] = edge1[2] * edge2[0] - edge1[0] * edge2[2]
-  n[2] = edge1[0] * edge2[1] - edge1[1] * edge2[0]
-
-  return n
-}
-
-const getNormal = (v1, v2, v3) => {
-  let edge1 = new Array(3)
-  edge1[0] = v2[0] - v1[0]
-  edge1[1] = v2[1] - v1[1]
-  edge1[2] = v2[2] - v1[2]
-
-  let edge2 = new Array(3)
-  edge2[0] = v3[0] - v1[0]
-  edge2[1] = v3[1] - v1[1]
-  edge2[2] = v3[2] - v1[2]
-
-  return cross(edge1, edge2)
-}
-
 class Sphere {
   constructor(nDiv, radius) {
     this.vertices = []
@@ -118,8 +90,7 @@ class Sphere {
         let z = Math.cos(theta)
 
         this.vertices.push(radius * x, radius * y, radius * z)
-        // Inizializzo tutte le normali a 0.
-        this.normals.push(0.0, 0.0, 0.0)
+        this.normals.push(x, y, z)
       }
     }
 
@@ -133,34 +104,9 @@ class Sphere {
 
         // I punti vanno uniti come nel cilindro per formare dei quadrati.
         this.indices.push(p1, p2, p1 + 1)
-        this.addNormal(p1, p2, p1 + 1)
-
         this.indices.push(p1 + 1, p2, p2 + 1)
-        this.addNormal(p1 + 1, p2, p2 + 1)
       }
     }
-  }
-
-  getVertex(idx) {
-    // Dato un indice ritorna il vertice.
-    return [this.vertices[3 * idx], this.vertices[3 * idx + 1], this.vertices[3 * idx + 2]]
-  }
-
-  addNormal(idx1, idx2, idx3) {
-    // Array di 3 componenti.
-    let normal = getNormal(this.getVertex(idx1), this.getVertex(idx2), this.getVertex(idx3))
-
-    this.normals[3 * idx1] += normal[0]
-    this.normals[3 * idx1 + 1] += normal[1]
-    this.normals[3 * idx1 + 2] += normal[2]
-
-    this.normals[3 * idx2] += normal[0]
-    this.normals[3 * idx2 + 1] += normal[1]
-    this.normals[3 * idx2 + 2] += normal[2]
-
-    this.normals[3 * idx3] += normal[0]
-    this.normals[3 * idx3 + 1] += normal[1]
-    this.normals[3 * idx3 + 2] += normal[2]
   }
 }
 
